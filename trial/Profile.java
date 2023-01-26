6d634d29e1aa003bd2183cfb7ea5eb72bbc92b72
Ahmad Abou Saleh <ahmad.abou.saleh24@gmail.com>
2023-01-26 20:48:13 +0200

    my hobby is added

--- trial/Profile.java
+++ trial/Profile.java
@@ -1,6 +1,6 @@
 public class Profile{
 	public static void main{
 		System.out.println("Ahmad Abou Saleh, Electrical Engineering, First year.";
-
+             System.out.println("my hobby is playing BasketBall");
 	}
 }
