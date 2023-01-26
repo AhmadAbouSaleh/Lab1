edae3cde00e50c24f95c394db768e75b9465b9d6
Ahmad Abou Saleh <ahmad.abou.saleh24@gmail.com>
2023-01-26 20:30:50 +0200

    problems has been fixed

---
+++ trial/Problem.java
@@ -0,0 +1,8 @@
+public class Problem{
+	public static void main(String[]args){
+		System.out.print("hi, can we check this? ");
+		System.out.println("it has a lot of problems");
+		System.out.println("yeah, i know!!!");
+// this is a comment
+	}
+}
